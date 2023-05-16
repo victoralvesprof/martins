075e9a09c6ce409abeee0565ba98c6437d048e30
vhcalves <victoralves.prof@gmail.com>
2023-05-16 09:22:53 -0300

    update tsc

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://vhcalves:Root_1234@cluster0.3hzro9z.mongodb.net/martins-clientes");
+mongoose.connect("mongodb+srv://vhcalves:Root_1234@cluster0.3hzro9z.mongodb.net/?retryWrites=true&w=majority");
 
-let db = mongoose.connection;
+const db = mongoose.connection;
 
 export default db;
