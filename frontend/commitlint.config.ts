6daf3e679c2aa527672d906c46939e3a437b658f
vhcalves <victoralves.prof@gmail.com>
2023-10-21 21:50:15 -0300

    feat: husky with commitlint

---
+++ frontend/commitlint.config.ts
@@ -0,0 +1,27 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'button',
+      ],
+    ],
+  },
+};
+
+export default Configuration;
